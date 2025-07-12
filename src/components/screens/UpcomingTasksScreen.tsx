5921a531ace9f8ce29caa622afb48ad01212966e
Muhammad Sulaiman <218628972+mk-slmn@users.noreply.github.com>
2025-07-12 14:35:10 +0100

    add placeholder text to screens

--- src/components/screens/UpcomingTasksScreen.tsx
+++ src/components/screens/UpcomingTasksScreen.tsx
@@ -1,5 +1,9 @@
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 
 export default function UpcomingTasksScreen() {
-  return <View></View>;
+  return (
+    <View className="flex-1 items-center justify-center bg-lime-50">
+      <Text className="text-3xl font-medium text-gray-500">Tasks Screen</Text>
+    </View>
+  );
 }
