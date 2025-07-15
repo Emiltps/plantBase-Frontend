1afe3c025e8587dde94520539a6113ecb028cbea
Muhammad Sulaiman <218628972+mk-slmn@users.noreply.github.com>
2025-07-15 13:57:20 +0100

    feat: add HomeStackNav

---
+++ src/components/navigation/HomeStackNav.tsx
@@ -0,0 +1,14 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen';
+import AddPlantScreen from '../screens/AddPlantScreen';
+
+const Stack = createNativeStackNavigator();
+
+export default function HomeStackNav() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="HomeMain" component={HomeScreen} options={{ headerShown: false }} />
+      <Stack.Screen name="AddPlant" component={AddPlantScreen} options={{ title: 'Add Plant' }} />
+    </Stack.Navigator>
+  );
+}
