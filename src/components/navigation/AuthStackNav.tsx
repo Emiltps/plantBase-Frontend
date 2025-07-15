35ded2e083c52918f4fcf1646d61dc8ede508545
Maiwand Ayubi <maiiiwand@gmail.com>
2025-07-15 14:26:20 +0100

    config: GetStart screen as default

--- src/components/navigation/AuthStackNav.tsx
+++ src/components/navigation/AuthStackNav.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import LoginScreen from '../screens/LoginScreen';
 import SignupScreen from '../screens/SignupScreen';
+import GetStartedScreen from '../screens/GetStartedScreen';
 
 const Stack = createNativeStackNavigator();
 
 export default function AuthStackNav() {
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
+    <Stack.Navigator initialRouteName="GetStarted" screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="GetStarted" component={GetStartedScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="SignUp" component={SignupScreen} />
     </Stack.Navigator>
