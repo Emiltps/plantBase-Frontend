6776e3c05487d1cf3d78764ab7fb813ddd76ea9d
Maiwand Ayubi <maiiiwand@gmail.com>
2025-07-15 12:47:32 +0100

    feature: add scroll when keyboard is in display

--- src/components/screens/ProfileScreen.tsx
+++ src/components/screens/ProfileScreen.tsx
@@ -7,6 +7,9 @@ import {
   Image,
   Alert,
   ActivityIndicator,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useAuth } from '../../contexts/AuthContext';
@@ -70,59 +73,66 @@ export default function ProfileScreen() {
   }
 
   return (
-    <SafeAreaView edges={['top']} className="flex-1 bg-white px-6 pt-4">
-      <Text className="mb-8 mt-6 text-center text-3xl">Your Profile</Text>
+    <KeyboardAvoidingView
+      style={{ flex: 1 }}
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      keyboardVerticalOffset={Platform.OS === 'ios' ? 0 : 20}>
+      <SafeAreaView edges={['top']} className="flex-1 bg-white px-6 pt-4">
+        <ScrollView contentContainerStyle={{ flexGrow: 1 }} keyboardShouldPersistTaps="handled">
+          <Text className="mb-8 mt-6 text-center text-3xl">Your Profile</Text>
 
-      <View className="mb-8 items-center">
-        <Image
-          source={avatarUrl ? { uri: avatarUrl } : require('../../../assets/dummy_profile.png')}
-          className="h-48 w-48 rounded-full"
-        />
-      </View>
+          <View className="mb-8 items-center">
+            <Image
+              source={avatarUrl ? { uri: avatarUrl } : require('../../../assets/dummy_profile.png')}
+              className="h-48 w-48 rounded-full"
+            />
+          </View>
 
-      <Text className="mb-1 text-sm text-gray-700">Full Name</Text>
-      <TextInput
-        value={fullName}
-        onChangeText={setFullName}
-        placeholder="Enter your full name"
-        className="mb-4 rounded-md border-2 border-gray-300 px-4 py-4 text-lg"
-      />
+          <Text className="mb-1 text-sm text-gray-700">Full Name</Text>
+          <TextInput
+            value={fullName}
+            onChangeText={setFullName}
+            placeholder="Enter your full name"
+            className="mb-4 rounded-md border-2 border-gray-300 px-4 py-4 text-lg"
+          />
 
-      {/* Username */}
-      <Text className="mb-1 text-sm text-gray-700">Username</Text>
-      <TextInput
-        value={username}
-        editable={false}
-        placeholder="Choose a username"
-        className="mb-4 rounded-md border-2 border-gray-300 bg-gray-100 px-4 py-4 text-lg"
-      />
+          {/* Username */}
+          <Text className="mb-1 text-sm text-gray-700">Username</Text>
+          <TextInput
+            value={username}
+            editable={false}
+            placeholder="Choose a username"
+            className="mb-4 rounded-md border-2 border-gray-300 bg-gray-100 px-4 py-4 text-lg"
+          />
 
-      <Text className="mb-1 text-sm text-gray-700">Email Address</Text>
-      <TextInput
-        value={email}
-        onChangeText={setEmail}
-        placeholder="Enter your email"
-        keyboardType="email-address"
-        autoCapitalize="none"
-        className="mb-4 rounded-md border-2 border-gray-300 px-4 py-4 text-lg"
-      />
+          <Text className="mb-1 text-sm text-gray-700">Email Address</Text>
+          <TextInput
+            value={email}
+            onChangeText={setEmail}
+            placeholder="Enter your email"
+            keyboardType="email-address"
+            autoCapitalize="none"
+            className="mb-4 rounded-md border-2 border-gray-300 px-4 py-4 text-lg"
+          />
 
-      <TouchableOpacity
-        onPress={handleSave}
-        disabled={loading}
-        className="mb-4 items-center rounded-md bg-green-500 py-4">
-        {loading ? (
-          <ActivityIndicator color="#fff" />
-        ) : (
-          <Text className="font-large text-lg font-bold text-white">Save</Text>
-        )}
-      </TouchableOpacity>
+          <TouchableOpacity
+            onPress={handleSave}
+            disabled={loading}
+            className="mb-4 items-center rounded-md bg-green-500 py-4">
+            {loading ? (
+              <ActivityIndicator color="#fff" />
+            ) : (
+              <Text className="font-large text-lg font-bold text-white">Save</Text>
+            )}
+          </TouchableOpacity>
 
-      <TouchableOpacity
-        onPress={() => signOut()}
-        className="items-center rounded-md border border-red-500 bg-red-600 py-4">
-        <Text className="font-large text-lg font-bold text-white">Log Out</Text>
-      </TouchableOpacity>
-    </SafeAreaView>
+          <TouchableOpacity
+            onPress={() => signOut()}
+            className="items-center rounded-md border border-red-500 bg-red-600 py-4">
+            <Text className="font-large text-lg font-bold text-white">Log Out</Text>
+          </TouchableOpacity>
+        </ScrollView>
+      </SafeAreaView>
+    </KeyboardAvoidingView>
   );
 }
