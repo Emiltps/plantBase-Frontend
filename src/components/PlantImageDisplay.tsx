ca6565acd23ed1bdd41962d7b28d93a44a62dc52
“Emiltpsgit config --global user.email “emiltps@yahoo.co.uk <emiltps@yahoo.co.uk>
2025-07-15 14:45:13 +0200

    add image component

--- src/components/PlantImageDisplay.tsx
+++ src/components/PlantImageDisplay.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Image, View } from 'react-native';
 
-const PlantImageDisplay = ({ imageUrl }: { imageUrl: string }) => {
+const PlantImageDisplay = ({ photo_url }: { photo_url: string }) => {
   return (
+    
     <View className="w-full h-64 bg-lime-100">
       <Image
-        source={{ uri: imageUrl }}
+        source={{ uri: photo_url }}
         className="w-full h-full"
         resizeMode="cover"
       />
--- src/components/screens/PlantDetailScreen.tsx
+++ src/components/screens/PlantDetailScreen.tsx
@@ -4,13 +4,26 @@ import { useNavigation } from '@react-navigation/native';
 import PlantInfoSection from '../PlantInfoSection';
 import PlantImageDisplay from '../PlantImageDisplay'
 
-const PlantDetailScreen = ({ route }) => {
+const mockPlant = 
+  {
+    owner_id: "12e3f4a5-b6c7-4d89-8e01-23f45c67d890",
+    plant_type_id: 4,
+    nickname: "Fernie",
+    photo_url: "https://www.houseplant.co.uk/cdn/shop/files/Boston_Fern_Green_Moment_Indoor_Tropical_Houseplant.jpg?v=1737121676",
+    profile_description: "A vibrant Boston fern thriving in the living room.",
+    notes: "Needs watering every 3 days.",
+    status: "alive",
+    created_at: "2024-05-01T10:30:00Z",
+    died_at: null,
+  };
+
+const PlantDetailScreen = () => {
   const navigation = useNavigation();
-  const { plant } = route.params;
+  const  plant  = mockPlant//route.params;
 
   return (
     <ScrollView className="flex-1 bg-white">
-         <PlantImageDisplay imageUrl={plant.imageUrl} />
+         <PlantImageDisplay photo_url={plant.photo_url} />
 
       <View className="px-4 py-6">
         <PlantInfoSection plant={plant} />
