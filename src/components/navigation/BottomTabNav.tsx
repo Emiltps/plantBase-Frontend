578fd37b1d9eafa26437fb8bff3eb52bc628d16a
Maiwand Ayubi <maiiiwand@gmail.com>
2025-07-15 10:19:08 +0100

    changed styling

--- src/components/navigation/BottomTabNav.tsx
+++ src/components/navigation/BottomTabNav.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import HomeScreen from '../screens/HomeScreen';
 import UpcomingTasksScreen from '../screens/UpcomingTasksScreen';
-import CalendarScreen from '../screens/CalendarScreen';
 import ProfileScreen from '../screens/ProfileScreen';
 import { Ionicons } from '@expo/vector-icons';
 
@@ -16,6 +15,26 @@ export default function BottomTabNav() {
         headerShown: false,
         tabBarActiveTintColor: '#4CAF50',
         tabBarInactiveTintColor: 'gray',
+        tabBarStyle: {
+          position: 'absolute',
+          bottom: 16,
+          marginHorizontal: 16,
+          marginBottom: 8,
+          backgroundColor: '#ffffff',
+          borderWidth: 2,
+          borderColor: 'gray',
+          shadowColor: 'transparent',
+          shadowOpacity: 0,
+          shadowOffset: { width: 0, height: 0 },
+          borderTopWidth: 2,
+          borderRadius: 16,
+          height: 72,
+          paddingTop: 8,
+        },
+        tabBarItemStyle: {
+          alignItems: 'center',
+          justifyContent: 'center',
+        },
 
         tabBarIcon: ({ color, size }) => {
           let iconName;
@@ -25,9 +44,6 @@ export default function BottomTabNav() {
               break;
             case 'Tasks':
               iconName = 'bookmark-outline';
-              break;
-            case 'Calendar':
-              iconName = 'calendar-outline';
               break;
             case 'Profile':
               iconName = 'person-outline';
@@ -40,7 +56,6 @@ export default function BottomTabNav() {
       })}>
       <Tab.Screen name="Home" component={HomeScreen} />
       <Tab.Screen name="Tasks" component={UpcomingTasksScreen} />
-      <Tab.Screen name="Calendar" component={CalendarScreen} />
       <Tab.Screen name="Profile" component={ProfileScreen} />
     </Tab.Navigator>
   );
