03867ff050729c9e1f45f34c67d43def53c042a6
Muhammad Sulaiman <218628972+mk-slmn@users.noreply.github.com>
2025-07-16 09:52:51 +0100

    feat: display user first name and date on home screen

--- src/components/screens/HomeScreen.tsx
+++ src/components/screens/HomeScreen.tsx
@@ -4,6 +4,8 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import { getAllPlants } from '../../../api/plants';
 import PlantPreviewCard from '../PlantPreviewCard';
 import { useNavigation } from '@react-navigation/native';
+import { format } from 'date-fns';
+import { useAuth } from '../../contexts/AuthContext';
 
 type Plant = {
   plant_id: number;
@@ -15,7 +17,10 @@ type Plant = {
 export default function HomeScreen() {
   const [plants, setPlants] = useState<Plant[]>([]);
 
+  const { user } = useAuth();
   const navigation = useNavigation() as any;
+  const firstName = user?.user_metadata?.full_name?.split(' ')[0] || 'there';
+  const todayFormatted = format(new Date(), "'Today is' EEEE, do 'of' MMMM");
 
   useEffect(() => {
     async function fetchPlants() {
@@ -32,7 +37,8 @@ export default function HomeScreen() {
   return (
     <SafeAreaView className="flex-1 bg-lime-50">
       <View className="px-4 py-6">
-        <Text className="mb-2 text-xl font-semibold">Welcome back! 🌿</Text>
+        <Text className="mb-2 text-xl font-semibold">Welcome back, {firstName || 'there'}! 🌿</Text>
+        <Text className="mt-1 text-gray-600">{todayFormatted}</Text>
         <Text className="text-md text-gray-600">
           You have {plants.length} plant{plants.length === 1 ? '' : 's'}
         </Text>
@@ -48,11 +54,7 @@ export default function HomeScreen() {
         data={plants}
         keyExtractor={(item) => item.plant_id.toString()}
         contentContainerStyle={{ paddingHorizontal: 16 }}
-        renderItem={({ item }) => (
-          <PlantPreviewCard
- plant={item} 
-          />
-        )}
+        renderItem={({ item }) => <PlantPreviewCard plant={item} />}
       />
     </SafeAreaView>
   );
