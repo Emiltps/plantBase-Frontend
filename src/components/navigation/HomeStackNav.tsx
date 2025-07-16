c8bce28106252eee06d41cf45fc23556d1c0909f
Maiwand Ayubi <maiiiwand@gmail.com>
2025-07-16 13:54:02 +0100

    styling: implement styling for HomeStackNav

--- src/components/navigation/HomeStackNav.tsx
+++ src/components/navigation/HomeStackNav.tsx
@@ -1,3 +1,5 @@
+import { Ionicons } from '@expo/vector-icons';
+import { TouchableOpacity, Text, View } from 'react-native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from '../screens/HomeScreen';
 import AddPlantScreen from '../screens/AddPlantScreen';
@@ -9,18 +11,28 @@ const Stack = createNativeStackNavigator();
 
 export default function HomeStackNav() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator
+      screenOptions={{
+        header: ({ navigation }) => (
+          <View className="flex-row items-center bg-white px-4 pb-8 pt-[60px]">
+            <TouchableOpacity
+              onPress={() => navigation.goBack()}
+              className="flex-row items-center rounded-full border border-2 border-gray-300 bg-white p-1">
+              <View className="h-[58px] w-[58px] items-center justify-center rounded-full bg-[#f8f8f8]">
+                <Ionicons name="arrow-back" size={20} color="#306739" />
+              </View>
+              <Text className="text-main-text mx-6 text-lg font-semibold">Go Back</Text>
+            </TouchableOpacity>
+          </View>
+        ),
+      }}>
       <Stack.Screen
         name="HomeMain"
         component={HomeScreen}
         options={{ headerShown: false, title: 'Home' }}
       />
-      <Stack.Screen name="AddPlant" component={AddPlantScreen} options={{ title: 'Add Plant' }} />
-      <Stack.Screen
-        name="PlantDetailScreen"
-        component={PlantDetailScreen}
-        options={{ title: 'Plant Details' }}
-      />
+      <Stack.Screen name="AddPlant" component={AddPlantScreen} />
+      <Stack.Screen name="PlantDetailScreen" component={PlantDetailScreen} />
       <Stack.Screen name="EditScheduleScreen" component={EditTaskScreen} />
       <Stack.Screen name="UpcomingTasksScreen" component={UpcomingTasksScreen} />
     </Stack.Navigator>
